49bb692acc652a449b1db4f3d08bec2a2a28fb23
meng <Annielm1996@gmail.com>
2017-12-01 17:34:44 -0500

    login

--- ecom/urls.py
+++ ecom/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^account/', include('ecom.apps.account.urls',namespace = 'account')),
     url(r'^cart/',include('ecom.apps.cart.urls',namespace = 'cart')),
     url(r'^', include('ecom.apps.catalog.urls',namespace = 'catalog')),
 
